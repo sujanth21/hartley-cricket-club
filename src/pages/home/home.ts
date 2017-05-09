b913828e79e4f523cfc7f5fbda68ef7901a6c9ae
Sujanth <sujanth1987@gmail.com>
2017-05-09 23:39:07 +1000

    data added from VSCA

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
-  }
+  slides = [
+    {
+      title: "Love Cricket? Join HCC!",
+      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+      image: "assets/images/cricket1.png",
+    },
+    {
+      title: "Build Rapport!",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "assets/images/cricket2.png",
+    },
+    {
+      title: "Meet New Friends!",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "assets/images/cricket3.png",
+    }
+  ];
 
 }
